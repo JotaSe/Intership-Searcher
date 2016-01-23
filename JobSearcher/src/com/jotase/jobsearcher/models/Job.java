a3b3c7c6a3b61a6d1f47b1842e48f95a59f72dcf
Javier\JotaSe <jota.segovia@gmail.com>
2016-01-23 20:21:56 -0300

    initial commit

---
+++ JobSearcher/src/com/jotase/jobsearcher/models/Job.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.jotase.jobsearcher.models;
+
+/**
+ *
+ * @author JotaSe
+ */
+public class Job extends Metadata{
+    private String title;
+    private String description;
+    
+}
